434581839beb8831c73c3d5810fd1bdc79258b62
Ian Martinez <imartinez0753@gmail.com>
2020-08-09 12:03:05 -0700

    db and server created

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Double-Double', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Vegi-Burger', flase);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('BaconBurger', true);
+
+
